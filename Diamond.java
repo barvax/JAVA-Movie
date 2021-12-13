8d38e955a43255472a89892746c30891c4a77371
barvax <69000340+barvax@users.noreply.github.com>
2021-12-13 08:21:54 +0200

    test

--- Diamond.java
+++ Diamond.java
@@ -1,18 +1,19 @@
 package com.company;
 import java.util.Arrays;
-public class Diamond {
+public class Main {
 
-    int[] ron = {58,7,3,1,2,40,10,2};   ///{58,7,6,1,3,40,10,2}
+   
+    public static void main(String[] args) {
+        System.out.println("Hello, World-!"); 
+        
+        int[] ron = {58,7,1,1,3,40,10,2};   ///{58,8,6,1,3,40,10,2}
         int max =findMax(ron);
-      // System.out.println("max");
+       System.out.println(max);
     int answer = z(ron);
-       // System.out.println(answer);
-
-
-
-
-
-    public  int z (int [] arr){
+       System.out.println(answer);
+    }
+    
+    public static  int z (int [] arr){
         int max =  findMax(arr);
         int min = max;
         int min1 = max;
@@ -28,7 +29,7 @@ public class Diamond {
         int total1 = max;
         int total2 = max;
         for (int i = 1; i < arr.length-1; i++) {
-            if(arr[i]<min){
+            if(arr[i]<=min){
                 min = arr[i];
                 index1 = i;
                 min1 = arr[i];
@@ -37,7 +38,7 @@ public class Diamond {
         min = findMax(arr);
 
         for (int i = 1; i < arr.length-1; i++) {
-            if(arr[i]<min&&arr[i]!=arr[index1]&&arr[i]!=arr[index1-1]&&arr[i]!=arr[index1+1]){
+            if(arr[i]<=min&&i!=index1&&i!=index1-1&&i!=index1+1){
                 min = arr[i];
                 index2 = i;
                 min2 = arr[i];
@@ -48,15 +49,15 @@ public class Diamond {
         total1 = min1+min2;
         min = findMax(arr);
         for (int i = 1; i < arr.length-1; i++) {
-            if(arr[i]<min&&arr[i]!=arr[index1]){
+            if(arr[i]<=min&&i!=index1){
                 min = arr[i];
                 index3 = i;
                 min3 = arr[i];
             }
         }
-        min = findMax(arr);
+     min = findMax(arr);
         for (int i = 1; i < arr.length-1; i++) {
-            if(arr[i]<min&&arr[i]!=arr[index3]&&arr[i]!=arr[index3-1]&&arr[i]!=arr[index3+1]){
+            if(arr[i]<=min&&i!=index3&&i!=index3-1&&i!=index3+1){
                 min = arr[i];
                 index4 = i;
                 min4 = arr[i];
@@ -65,14 +66,14 @@ public class Diamond {
         System.out.println("min3=" + min3 +" index of min3=" +  index3);
         System.out.println("min4=" + min4 +" index of min4=" +  index4);
         total2 = min3+min4;
-        System.out.println("total 1=" + total1 +" total 2=" +  total2);
+        System.out.println("total of option 1=" + total1 +" total of option 2=" +  total2);
         if(total1<total2){
             return total1;
         }
         return total2;
     }
-
-    public  int findMax(int [] arr){
+    
+     public static int findMax(int [] arr){
         int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if(arr[i]>max){
@@ -81,4 +82,5 @@ public class Diamond {
         }
         return max;
     }
+
 }
